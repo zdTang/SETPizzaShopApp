4bdea14407d8fea185a9d35c171d0f938b0741b9
zdTang <44347399+zdTang@users.noreply.github.com>
2018-12-14 11:04:50 -0500

    add server.ashx

---
+++ SETPizzaShopApp/server.ashx.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SETPizzaShopApp
+{
+    /// <summary>
+    /// Summary description for server
+    /// </summary>
+    public class server : IHttpHandler
+    {
+
+        
+        public void ProcessRequest(HttpContext context)
+        {
+            string strType = context.Request.Params["type"];// parse parameters
+
+
+
+
+            context.Response.ContentType = "text/plain";
+            context.Response.Write("Hello World");
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
